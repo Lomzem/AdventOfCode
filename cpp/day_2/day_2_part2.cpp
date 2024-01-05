447f0dd01c1a1454173c1a8b658831b0cbc6e9a9
Lawjay <lawjaylee1@icloud.com>
2024-01-04 16:05:59 -0800

    got the right answer for day 2 part 2

--- cpp/day_2/day_2_part2.cpp
+++ cpp/day_2/day_2_part2.cpp
@@ -5,7 +5,6 @@
 #include <unordered_map>
 #include <vector>
 
-using std::cout;
 using std::string;
 using std::unordered_map;
 using std::vector;
@@ -13,16 +12,30 @@ using std::vector;
 class DiceGame {
   public:
     string text;
-    vector<unordered_map<string, int>> roundColors;
+    vector<unordered_map<string, int>> roundsColors;
 
     DiceGame(string gameText) {
         text = gameText;
-        parse_game("Game 1 : 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+        parse_game(text);
+        getColorMaxes();
+    }
+
+    unordered_map<string, int> getColorMaxes() {
+        unordered_map<string, int> colorMaxes;
+        for (unordered_map<string, int> roundColor : roundsColors) {
+            for (auto const &[key, val] : roundColor) {
+                if (colorMaxes.find(key) == colorMaxes.end() ||
+                    colorMaxes.at(key) < val) {
+                    colorMaxes[key] = val;
+                }
+            }
+        }
+        return colorMaxes;
     }
 
   private:
     void parse_game(string gameText) {
-        vector<unordered_map<string, int>> roundColors;
+        // vector<unordered_map<string, int>> roundsColors;
         string roundInfo = gameText.substr(gameText.find(":") + 1);
 
         size_t pos = 0;
@@ -30,11 +43,11 @@ class DiceGame {
 
         while ((pos = roundInfo.find(roundDelim)) != string::npos) {
             string round = roundInfo.substr(1, pos - 1);
-            roundColors.push_back(parse_round(round));
+            roundsColors.push_back(parse_round(round));
             roundInfo.erase(0, pos + roundDelim.length());
         }
         string round = roundInfo.substr(1, pos - 1);
-        roundColors.push_back(parse_round(round));
+        roundsColors.push_back(parse_round(round));
     }
 
     unordered_map<string, int> parse_round(string roundText) {
@@ -65,13 +78,16 @@ int part_2(string filename) {
     std::ifstream file(filename);
     int sum = 0;
 
-    int i = 0;
     while (std::getline(file, line)) {
-        if (i == 0) {
-            DiceGame lineGame(line);
+        DiceGame lineGame(line);
+        unordered_map<string, int> colorMaxes = lineGame.getColorMaxes();
+        int power = 1;
+        for (auto i : colorMaxes) {
+            power *= i.second;
         }
-        i++;
+        sum += power;
     }
+    std::cout << sum << "\n";
     return sum;
 }
 
